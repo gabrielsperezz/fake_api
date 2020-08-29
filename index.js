7bbe19dbc493b9dddf2476a3f56785674397203c
Gabriel Perez <gabriel.perez@tray.net.br>
2020-08-29 08:37:39 -0300

    MElhoras

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const PORT = 8000;
 const app = express();
 
 app.use(bodyParser.json());
-app.post('/messages', function(req, res) {
+app.post('/webhook', function(req, res) {
   console.log('webhook PAYLOAD:\n', JSON.stringify(req.body, null, 4));
+  res.status(204);
+  res.send();
 });
 
 // Listen on port
